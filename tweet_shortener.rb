1307b688e1a5d60c7952b948e7b263658aea471d
Jonathan Biro <jonnybrx@gmail.com>
2018-11-18 23:48:12 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,10 +40,10 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  shortened_tweet = word_substituter(tweet)
-  if word_substituter(tweet).length > 140
-    word_substituter[0...140]
-  else
-    shortened_tweet
-  end
+if word_substituter(tweet).length > 140
+  word_substituter(tweet)[0..136] + '...'
+else
+  tweet
+end
+
 end
