3c2b7733c102d566cb21f92c9f4435a64018876b
Jonathan Biro <jonnybrx@gmail.com>
2018-11-18 23:46:32 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,49 @@
-# Write your code here.
+require 'pry'
+
+def dictionary 
+  replacement = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+     tweet.split.collect do |word|
+      if dictionary.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(arr)
+  arr.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+    if(tweet.length > 140)
+      word_substituter(tweet)
+    else
+      tweet
+    end
+end
+
+def shortened_tweet_truncator(tweet)
+  short_tweet = word_substituter(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter[0...140]
+  else
+    tweet
+  end
+end
